89f5bfbc1d8718d672f2ed4f325ebff605237525
Adam Gray <adamgray@Adams-MacBook-Pro.local>
2023-08-28 14:40:19 -0500

    test

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,17 +1,22 @@
 import { defineStackbitConfig } from '@stackbit/types';
-import { NetlifyConnectSource } from './stackbit/NetlifyConnectSource';
+import { ContentfulContentSource } from '@stackbit/cms-contentful'
 
 export default defineStackbitConfig({
   stackbitVersion: '~0.6.0',
   ssgName: 'nextjs',
   nodeVersion: '16',
   contentSources: [
-    new NetlifyConnectSource({
-      projectId: 'netlify_full_stack_demo_source',
-      apiKey: 'test'
-    })
+    new ContentfulContentSource({
+      spaceId: "m7021t7gk76q",
+      environment: "master",
+      previewToken: "2cCShdcXwdCuKk_ggzHjqdfjTxbgsQRaQcuMg0fjx2M",
+      accessToken: "CFPAT-0cMwF6NXgFq_wnW4Yw2uHHmmfUeOgVu7541KXbmH1AU",
+    }),
   ],
   models: {
     page: { type: 'page', urlPath: '/{slug}' },
   },
+  modelExtensions: [
+    { name: 'post', type: 'page', urlPath: '/posts/{slug}' },
+  ],
 })
